22c78ae449f07a71815b1796d1f0bd575ef8767d
bradpower7 <bradpower709@gmail.com>
2016-07-09 13:19:26 -0230

    Added dnStream class and turret.cpp main

---
+++ src/dnStream.cpp
@@ -0,0 +1,4 @@
+#include "dnStream.hpp"
+
+
+
---
+++ src/turret.cpp
@@ -0,0 +1,31 @@
+#include "turret.h"
+
+
+
+int main(int argc, char *argv[]){
+    
+    // 
+    
+    while(1){
+        
+        // Take in set of class/cordinate/probability pairs
+        // (calls Darknet code)
+        
+        
+        // If matches target class && If meets threshold 
+        // && If safety conditions satisfied:
+            // Calculate required motor movement
+            // Send a "MOVE" message to Arduino
+            
+            // Calculate distance between target class cordinates and 
+            // center of video stream (assumes calibration between camera and line of fire)
+            
+            // If distance < threshold:
+                // Send "FIRE" message to Arduino
+        
+        
+        
+    }
+    
+    
+}
